f972e2edc46b8cb0400c21dedb9b8162df73626b
Theanh130124 <theanhtran13012004@gmail.com>
2025-01-01 02:22:06 +0700

    JDBCDemo

---
+++ UnitTestDemo/src/main/java/com/trantheanh1301/services/JdbcDemo.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.trantheanh1301.services;
+
+/**
+ *
+ * @author LAPTOP
+ */
+public class JdbcDemo {
+    public static void main (String [] agrs){
+        //Jdbc tương tác được nhiều CSDL
+        
+        //B1 . Nạp driver
+        
+        //B2. Mở kết nối
+        
+        //B3.Thực thi truy vấn
+        
+        //B4.Đóng kết nối
+        
+    }
+    
+}
