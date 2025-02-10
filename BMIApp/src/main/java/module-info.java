5a42ceea3554fb56e3e911192dfb3e7bb1374ebc
Theanh130124 <theanhtran13012004@gmail.com>
2025-02-10 19:38:13 +0700

    Update First

--- BMIApp/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.trantheanh1301.bmiapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.trantheanh1301.bmiapp to javafx.fxml;
-    exports com.trantheanh1301.bmiapp;
-}
